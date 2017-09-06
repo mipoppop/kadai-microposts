c78ade10a5083ba8b8cb7d9c898f4f193b717969
mipoppop <mipoppop@hotmail.com>
2017-09-06 13:54:08 +0000

    follow, unfollow

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,9 @@ class Micropost < ApplicationRecord
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
 end
